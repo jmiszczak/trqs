fe0340b891901cf4b25b4ad79c9cc5c157f27b7a
Jarek Miszczak <jarek@miszczak.eu>
2012-08-15 13:02:00 +0200

    some tests with segfault Quantis problem in TRQS

--- programming/mathematica/TRQS/TRQS_Quantis/quantis_test.c
+++ programming/mathematica/TRQS/TRQS_Quantis/quantis_test.c
@@ -6,36 +6,24 @@
 #include "TRQS.h"
 
 int main(int argc, char* argv[]) {
-    double d;
-    
-    printf("DBL_MIN=%e\n",DBL_MIN);
-    printf("DBL_MAX=%e\n",DBL_MAX);
-    
-    if (!QuantisReadScaledDouble(QUANTIS_DEVICE_USB, 0, &d, 0, 1) ) {
-       printf("%e\n",d);
-    }
-    
-    printf ("%f\n", QuantisGetLibVersion());	
-    printf ("%s\n", QuantisGetSerialNumber(QUANTIS_DEVICE_USB,0));	
-    
-    
-    char* deviceType;
-    switch (QUANTIS_DEVICE_TYPE) {
-        case QUANTIS_DEVICE_USB:
-            deviceType = "USB";
-            break;
-        case QUANTIS_DEVICE_PCI:
-            deviceType = "PCI";
-            break;
-        default:
-            deviceType = "N/A";
-            break;
-    }
-    printf("%s\n", deviceType);
-    printf("%i\n", QUANTIS_DEVICE_TYPE);
-    
-    
-//    if ()
-    
-    return 0;
+  double d;
+  int noDevs;
+
+  printf("DBL_MIN=%e\n",DBL_MIN);
+  printf("DBL_MAX=%e\n",DBL_MAX);
+
+
+
+  if (!QuantisReadScaledDouble(QUANTIS_DEVICE_USB, 0, &d, 0, 1) ) {
+	printf("Random double: %e\n",d);
+  }
+
+
+  printf ("%f\n", QuantisGetLibVersion());	
+  printf ("%s\n", QuantisGetSerialNumber(QUANTIS_DEVICE_USB,0));	
+
+  printf ("Number of USB devices: %i\n",QuantisCount(QUANTIS_DEVICE_USB));
+  printf ("Number of PCI devices: %i\n",QuantisCount(QUANTIS_DEVICE_PCI));
+
+  return 0;
 }
