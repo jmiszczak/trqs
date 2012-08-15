41ce6315a2a8b0e13b305c655024e1b4e061e2a6
Jarek Miszczak <jarek@miszczak.eu>
2012-08-15 22:14:32 +0200

    bug report for libusb-1.0.9 problem in TRQS

--- programming/mathematica/TRQS/TRQS_Quantis/quantis_test.c
+++ programming/mathematica/TRQS/TRQS_Quantis/quantis_test.c
@@ -12,15 +12,12 @@ int main(int argc, char* argv[]) {
   printf("DBL_MIN=%e\n",DBL_MIN);
   printf("DBL_MAX=%e\n",DBL_MAX);
 
-
+  printf ("%f\n", QuantisGetLibVersion());	
+  printf ("%s\n", QuantisGetSerialNumber(QUANTIS_DEVICE_USB,0));	
 
   if (!QuantisReadScaledDouble(QUANTIS_DEVICE_USB, 0, &d, 0, 1) ) {
 	printf("Random double: %e\n",d);
   }
-
-
-  printf ("%f\n", QuantisGetLibVersion());	
-  printf ("%s\n", QuantisGetSerialNumber(QUANTIS_DEVICE_USB,0));	
 
   printf ("Number of USB devices: %i\n",QuantisCount(QUANTIS_DEVICE_USB));
   printf ("Number of PCI devices: %i\n",QuantisCount(QUANTIS_DEVICE_PCI));
